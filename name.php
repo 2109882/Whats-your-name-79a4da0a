22ee1c6a8689fe6adf58af6455a2208ab197d6dc
2109882 <jessie.algra@gmail.com>
2019-11-01 18:20:36 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "What is your name?\n";
+$input = readline();
+
+
+echo "what is your last name?\n";
+$input2 = readline();
+echo "Your name is: " . $input . " " . $input2 . "\n";
+
+
+?>
